c2ae4fd91c749aec1955d443826a35e85324c078
mylo <milopadma@yahoo.com>
2024-09-09 17:19:54 +0800

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from "./app.module";
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3692;
-  await app.listen(port);
-  console.log(`Server is running on port ${port}`);
+
+  // Enable CORS for all routes
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    allowedHeaders: "*",
+  });
+
+  await app.listen(3690 || process.env.PORT);
 }
 
 bootstrap().catch((err) => {
