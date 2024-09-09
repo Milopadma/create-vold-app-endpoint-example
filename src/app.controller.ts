697df657ef440dd8ca515dfafd40af429d809b55
mylo <milopadma@yahoo.com>
2024-09-09 12:02:20 +0800

    updated routes

--- src/app.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller, Get, Param } from "@nestjs/common";
-
-@Controller("v1")
-export class AppController {
-  // moved out
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PageController } from "./endpoints/page.controller";
 import { HomepageController } from "./endpoints/homepage.controller";
+import { WebconfigController } from "./endpoints/webconfig.controller";
 
 @Module({
   imports: [],
-  controllers: [PageController, HomepageController],
+  controllers: [PageController, HomepageController, WebconfigController],
   providers: [],
 })
 export class AppModule {}
